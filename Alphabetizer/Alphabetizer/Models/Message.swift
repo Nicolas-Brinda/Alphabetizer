620379d9525d64cc8b761a590e26e7520198daba
Nicolas Brinda <nicolas.brinda7@gmail.com>
2026-07-06 13:07:13 +0200

    This Is My First Game And My Initial Commit Help Me Make it Better

---
+++ Alphabetizer/Alphabetizer/Models/Message.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+enum Message: String {
+    case instructions = "Place The Tiles In Alphabetical Order 🔤"
+    case tryAgain = "Haha You Didn't Win Better Luck Next Time 😈"
+    case youWin = "Well Done 👏 You win!🏆"
+}
